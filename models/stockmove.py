499b90f5b336134676c42ad7f4fd59bb4d206080
Vanwesel Etienne <etienne.vanwesel@gmail.com>
2026-06-15 14:45:42 +0200

    Add class stockmove and supplier

---
+++ models/stockmove.py
@@ -0,0 +1,44 @@
+
+from db.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Identity, ForeignKey
+from typing import TYPE_CHECKING
+import enum
+from sqlalchemy import Enum
+import datetime
+
+# if TYPE_CHECKING:
+#     from profil import Profiles
+#     from post import Posts
+
+
+class Status(enum.Enum):
+    DRAFT = 1
+    DONE = 2
+    CANCELLED = 3
+
+class Type(enum.Enum):
+    IN = 1
+    OUT = 2
+    TRANSFER = 3
+
+class Stockmove(Base):
+    __tablename__ = "stockmove"
+
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    source_warehouse_id: Mapped[int] = mapped_column(ForeignKey("warehouse.id"), nullable=False)
+    destination_warehouse_id: Mapped[int] = mapped_column(ForeignKey("warehouse.id"), nullable=False)
+    type: Mapped[Type] = mapped_column(Enum(Type), nullable=False)
+    status: Mapped[Status] = mapped_column(Enum(Status), nullable=False)
+    created_at: Mapped[datetime] = mapped_column()
+
+    def __repr__(self):
+        return (f"product_id = {self.product_id}, 
+            quantity = {self.quantity}, 
+            source_warehouse_id = {self.source_warehouse_id}, 
+            destination_warehouse_id = {self.destination_warehouse_id}, 
+            type = {self.type}, 
+            status = {self.status}, 
+            created_at = {self.created_at}")
---
+++ models/supplier.py
@@ -0,0 +1,20 @@
+
+from db.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Identity
+from typing import TYPE_CHECKING
+
+# if TYPE_CHECKING:
+#     from profil import Profiles
+#     from post import Posts
+
+
+class Supplier(Base):
+    __tablename__ = "supplier"
+
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"name = {self.name}, email= {self.email}"
