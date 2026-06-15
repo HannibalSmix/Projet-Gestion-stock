971bcadb08b025b82046f75821f596455eeced86
Etienne Vanwesel <e.vanwesel@gmail.com>
2026-06-15 22:01:36 +0200

    Add products crud

---
+++ alembic/crud/products_crud.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+from models.products import Products
+from sqlalchemy.exc import IntegrityError
+
+def create_product(session: Session, name: str, sku: str, active: bool = True):
+
+    product = Products(name=name, sku=sku, active=active)
+
+    try:
+        session.add(product)
+        session.commit()
+    except IntegrityError as e:
+        session.rollback()
+        print(f"Erreur : {e.orig}")
+
+    session.refresh(product)
+
+    return product
+
+def get_product(session: Session, product_id: int):
+    return session.get(Products, product_id)
+
+def get_product_by_sku(session: Session, sku: str):
+    return session.query(Products).filter(Products.sku == sku).first()
+
+def deactivate_product(session: Session, product_id: int):
+    
+    product = session.get(Products, product_id)
+    if not product:
+        return None
+    product.active = False
+
+    session.commit()
+    session.refresh(product)
+
+    return product
+
+def delete_product(session: Session, product_id: int):
+
+    product = session.get(Products, product_id)
+    if not product:
+        return False
+    
+    session.delete(product)
+    session.commit()
+
+    return True
