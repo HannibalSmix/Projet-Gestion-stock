1ed3b67d3dfd7f0b42f57951f6f71ce94f87dd8b
Vanwesel Etienne <etienne.vanwesel@gmail.com>
2026-06-15 15:03:36 +0200

    Add receiptline

---
+++ models/receiptline.py
@@ -0,0 +1,29 @@
+from db.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, Identity, ForeignKey
+from typing import TYPE_CHECKING
+
+# if TYPE_CHECKING:
+#     from profil import Profiles
+#     from post import Posts
+
+class ReceiptLine(Base):
+    __tablename__ = "receiptline"
+
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
+    receipt_id: Mapped[int] = mapped_column(ForeignKey("receipt.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("product.id"), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    def __repr__(self):
+        return (
+            f"receipt_id = {self.receipt_id},"
+            f"product_id = {self.product_id},"
+            f"quantity = {self.quantity}"
+        )
+
+
+
+
+
+
